32039007ff43282b37076d5a99602aa019633db3
Aymen <AymenMir1001@gmail.com>
2022-12-22 23:53:21 +0100

    Add market fees and supported tokens

--- smart_contracts/contracts/AARTMarket.sol
+++ smart_contracts/contracts/AARTMarket.sol
@@ -3,10 +3,14 @@
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "./interfaces/IAARTCollection.sol";
 
-contract AARTMarket is IERC721Receiver {
+contract AARTMarket is Ownable, IERC721Receiver {
+    uint256 private constant PRECISION = 1e3;
+    uint256 private constant MAX_FEE = 30; // 3% is maximum trade fee
+
     enum ListingStatus {
         Active,
         Sold,
@@ -58,6 +62,10 @@ contract AARTMarket is IERC721Receiver {
     Listing[] private _listings;
     Auction[] private _auctions;
 
+    // supported ERC20 tokens
+    address[] public supportedERC20tokens;
+    mapping(address => bool) private _erc20Tokensmapping;
+
     // tokenId => offer
     mapping(uint256 => Offer[]) private _offers;
 
@@ -66,6 +74,8 @@ contract AARTMarket is IERC721Receiver {
         private auctionBidderAmounts;
 
     IAARTCollection private immutable nftContract;
+
+    uint256 public fee = 10; // 1%
 
     //--------------------------------------------------------------------
     // EVENTS
@@ -87,6 +97,8 @@ contract AARTMarket is IERC721Receiver {
     event AuctionCanceled(uint256 auctionId);
     event AuctionEnded(uint256 auctionId, address buyer);
     event RoyaltyPaid(uint256 tokenId, address receiver, uint256 amount);
+    event NewSupportedToken(address token);
+    event NewFee(uint256 fee);
 
     //--------------------------------------------------------------------
     // ERRORS
@@ -113,7 +125,21 @@ contract AARTMarket is IERC721Receiver {
     error AARTMarket_HasNoBid(uint256 auctionId);
     error AARTMarket_AuctionPeriodNotEnded(uint256 auctionId, uint256 endTime);
     error AARTMarket_CancelImpossible(uint256 auctionId);
+    error AARTMarket_UnsupportedToken(address token);
+    error AARTMarket_AlreadySupported(address token);
+    error AARTMarket_InvalidFee(uint256 fee);
     error AARTMarket_TransferFailed();
+
+    //--------------------------------------------------------------------
+    // Modifiers
+
+    modifier allowedToken(address token) {
+        if (token != address(0)) {
+            if (!_erc20Tokensmapping[token])
+                revert AARTMarket_UnsupportedToken(token);
+        }
+        _;
+    }
 
     constructor(address _nftAddress) {
         if (_nftAddress == address(0)) revert AARTMarket_AddressZero();
@@ -128,7 +154,7 @@ contract AARTMarket is IERC721Receiver {
         uint256 _tokenId,
         address _paymentToken,
         uint256 _buyPrice
-    ) external returns (uint256 listingId) {
+    ) external allowedToken(_paymentToken) returns (uint256 listingId) {
         // check that the user is the owner of the token
         // also checks that token is from the AART collection
         if (nftContract.ownerOf(_tokenId) != msg.sender)
@@ -198,7 +224,7 @@ contract AARTMarket is IERC721Receiver {
         address paymentToken,
         uint256 offerPrice,
         uint256 expirationTime
-    ) external payable returns (uint256 offerId) {
+    ) external payable allowedToken(paymentToken) returns (uint256 offerId) {
         // revert if token does not exist in the AART collection
         _isAARTToken(tokenId);
 
@@ -286,7 +312,7 @@ contract AARTMarket is IERC721Receiver {
         uint256 _startPrice,
         uint128 _startTime,
         uint128 _endTime
-    ) external returns (uint256 auctionId) {
+    ) external allowedToken(_paymentToken) returns (uint256 auctionId) {
         // check that the user is the owner of the token
         // also checks that token is from the AART collection
         if (nftContract.ownerOf(_tokenId) != msg.sender)
@@ -362,6 +388,11 @@ contract AARTMarket is IERC721Receiver {
 
         if (_auctionStatus(_auctionId) != AuctionStatus.Open)
             revert AARTMarket_AuctionNotOpen(_auctionId);
+
+        if (
+            _auction.paymentToken == address(0) &&
+            msg.value != _auction.directBuyPrice
+        ) revert AARTMarket_InsufficientAmount();
 
         // handle payment
         _handleSalePayment(
@@ -490,6 +521,8 @@ contract AARTMarket is IERC721Receiver {
         (address royaltyReceiver, uint256 royaltyAmount) = nftContract
             .royaltyInfo(tokenId, salePrice);
 
+        uint256 feeAmount = (salePrice * fee) / PRECISION;
+
         if (paymentToken != address(0)) {
             // Case 1 : ERC20 payment
             if (seller != royaltyReceiver && royaltyAmount != 0) {
@@ -499,15 +532,34 @@ contract AARTMarket is IERC721Receiver {
                     royaltyReceiver,
                     royaltyAmount
                 );
-                // pay current item seller in ERC20 token
+
+                // pay platform fee
                 IERC20(paymentToken).transferFrom(
                     buyer,
-                    seller,
-                    salePrice - royaltyAmount
+                    address(this),
+                    feeAmount
                 );
+
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - royaltyAmount - feeAmount;
+                }
+                // pay current item seller in ERC20 token
+                IERC20(paymentToken).transferFrom(buyer, seller, finalAmount);
 
                 emit RoyaltyPaid(tokenId, royaltyReceiver, royaltyAmount);
             } else {
+                // pay platform fee
+                IERC20(paymentToken).transferFrom(
+                    buyer,
+                    address(this),
+                    feeAmount
+                );
+
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - feeAmount;
+                }
                 // seller is same as NFT creator so transfer directly
                 IERC20(paymentToken).transferFrom(buyer, seller, salePrice);
             }
@@ -516,10 +568,20 @@ contract AARTMarket is IERC721Receiver {
             if (seller != royaltyReceiver && royaltyAmount != 0) {
                 // pay NFT creator royalty in Matic
                 _sendMatic(royaltyReceiver, royaltyAmount);
+
                 // pay current item seller in Matic
-                _sendMatic(seller, salePrice - royaltyAmount);
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - royaltyAmount - feeAmount;
+                }
+                _sendMatic(seller, finalAmount);
+
                 emit RoyaltyPaid(tokenId, royaltyReceiver, royaltyAmount);
             } else {
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - feeAmount;
+                }
                 // seller is same as NFT creator so send directly
                 _sendMatic(seller, salePrice);
             }
@@ -535,19 +597,27 @@ contract AARTMarket is IERC721Receiver {
         (address royaltyReceiver, uint256 royaltyAmount) = nftContract
             .royaltyInfo(tokenId, salePrice);
 
+        uint256 feeAmount = (salePrice * fee) / PRECISION;
+
         if (paymentToken != address(0)) {
             // Case 1 : ERC20 payment
             if (seller != royaltyReceiver && royaltyAmount != 0) {
                 // pay NFT creator royalty in ERC20 token
                 IERC20(paymentToken).transfer(royaltyReceiver, royaltyAmount);
+
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - royaltyAmount - feeAmount;
+                }
                 // pay current item seller in ERC20 token
-                IERC20(paymentToken).transfer(
-                    seller,
-                    salePrice - royaltyAmount
-                );
+                IERC20(paymentToken).transfer(seller, finalAmount);
 
                 emit RoyaltyPaid(tokenId, royaltyReceiver, royaltyAmount);
             } else {
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - feeAmount;
+                }
                 // seller is same as NFT creator so transfer directly
                 IERC20(paymentToken).transfer(seller, salePrice);
             }
@@ -556,12 +626,24 @@ contract AARTMarket is IERC721Receiver {
             if (seller != royaltyReceiver && royaltyAmount != 0) {
                 // pay NFT creator royalty in Matic
                 _sendMatic(royaltyReceiver, royaltyAmount);
+
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - royaltyAmount - feeAmount;
+                }
+
                 // pay current item seller in Matic
-                _sendMatic(seller, salePrice - royaltyAmount);
+                _sendMatic(seller, finalAmount);
+
                 emit RoyaltyPaid(tokenId, royaltyReceiver, royaltyAmount);
             } else {
+                uint256 finalAmount;
+                unchecked {
+                    finalAmount = salePrice - feeAmount;
+                }
+
                 // seller is same as NFT creator so send directly
-                _sendMatic(seller, salePrice);
+                _sendMatic(seller, finalAmount);
             }
         }
     }
@@ -615,6 +697,10 @@ contract AARTMarket is IERC721Receiver {
         return _auctions;
     }
 
+    function getSupportedTokens() external view returns (address[] memory) {
+        return supportedERC20tokens;
+    }
+
     function getTokenBuyOffers(uint256 tokenId)
         external
         view
@@ -638,5 +724,24 @@ contract AARTMarket is IERC721Receiver {
         bytes calldata
     ) external pure override returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
+    }
+
+    // ********************** //
+    //     Owner functions    //
+    // ********************** //
+
+    function setFee(uint256 _fee) external onlyOwner {
+        if (_fee > MAX_FEE) revert AARTMarket_InvalidFee(_fee);
+        fee = _fee;
+
+        emit NewFee(_fee);
+    }
+
+    function addSupportedToken(address _token) external onlyOwner {
+        if (_erc20Tokensmapping[_token]) revert AARTMarket_AlreadySupported(_token);
+        _erc20Tokensmapping[_token] = true;
+        supportedERC20tokens.push(_token);
+
+        emit NewSupportedToken(_token);
     }
 }
