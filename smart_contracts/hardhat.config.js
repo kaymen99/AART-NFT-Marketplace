479fdfad8901c0be9e7d070b6b8da4de7a14f5e5
Aymen <AymenMir1001@gmail.com>
2022-12-22 23:51:08 +0100

    Update hardhat.config.js

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -29,6 +29,12 @@ module.exports = {
     compilers: [
       {
         version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
     ],
   },
