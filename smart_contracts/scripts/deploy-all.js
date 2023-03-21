cfcc41f007eca54389292d92ef740551d4c94b0b
Aymen <AymenMir1001@gmail.com>
2023-03-21 23:49:10 +0100

    Update deploy-all.js

--- smart_contracts/scripts/deploy-all.js
+++ smart_contracts/scripts/deploy-all.js
@@ -6,20 +6,30 @@ const { getAmountInWei, developmentChains } = require("../utils/helpers");
 
 async function main() {
   const deployNetwork = hre.network.name;
-
   const mintCost = getAmountInWei(10); // 10 matic
+
+  // Deploy AART Artists contract
+  const ArtistsContract = await ethers.getContractFactory("AARTArtists");
+  const artistsContract = await ArtistsContract.deploy();
+  await artistsContract.deployed();
 
   // Deploy AART NFT Collection contract
   const NFTContract = await ethers.getContractFactory("AARTCollection");
-  const nftContract = await NFTContract.deploy(mintCost);
+  const nftContract = await NFTContract.deploy(
+    artistsContract.address,
+    mintCost
+  );
   await nftContract.deployed();
+
+  // unpause NFT contract
+  await nftContract.pause(2);
 
   // Deploy AART market contract
   const MarketContract = await ethers.getContractFactory("AARTMarket");
   const marketContract = await MarketContract.deploy(nftContract.address);
-
   await marketContract.deployed();
 
+  console.log("AART Artists contract deployed at:\n", artistsContract.address);
   console.log("AART NFT contract deployed at:\n", nftContract.address);
   console.log("AART market ontract deployed at:\n", marketContract.address);
   console.log("Network deployed to :\n", deployNetwork);
@@ -30,8 +40,10 @@ async function main() {
     fse.copySync("./artifacts/contracts", "../front-end/src/artifacts");
     fs.writeFileSync(
       "../front-end/src/utils/contracts-config.js",
-      `export const marketContractAddress = "${marketContract.address}"
+      `
+      export const marketContractAddress = "${marketContract.address}"
       export const nftContractAddress = "${nftContract.address}"
+      export const artistsContractAddress = "${artistsContract.address}"
       export const ownerAddress = "${nftContract.signer.address}"
       export const networkDeployedTo = "${hre.network.config.chainId}"`
     );
