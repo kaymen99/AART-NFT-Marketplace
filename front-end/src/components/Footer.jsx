fdcc42b01d78560e783a8d31c23bab4fb9fb9a40
Aymen <AymenMir1001@gmail.com>
2023-04-05 23:57:53 +0100

    Update Footer.jsx

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -2,21 +2,50 @@ import React from "react";
 import "./../assets/styles/components/Footer.css";
 import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai";
 import { RiDiscordFill } from "react-icons/ri";
+import images from "../assets/images";
 
 function Footer() {
   return (
-    <div className="footer container">
-      <p>AART&#169; All Right Reserved</p>
-      <div className="social">
-        <a href="https://github.com/kaymen99">
-          <AiOutlineGithub size={20} color="#fff" />
-        </a>
-        <a href="https://github.com/kaymen99">
-          <AiOutlineTwitter size={20} color="#fff" />
-        </a>
-        <a href="https://github.com/kaymen99">
-          <RiDiscordFill size={20} color="#fff" />
-        </a>
+    <div className="footer section__padding">
+      <div className="footer-links">
+        <div className="footer-links_logo">
+          <div>
+            <img src={images.logo} width={100} />
+          </div>
+          <div>
+            <h3>Feel Free To Contact Me.</h3>
+          </div>
+        </div>
+        <div className="footer-links_div">
+          <h4>AART</h4>
+          <p>Explore</p>
+          <p>How it Works</p>
+          <p>Roadmap</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="footer-links_div">
+          <h4>Support</h4>
+          <p>Help center</p>
+          <p>Terms of service</p>
+          <p>Legal</p>
+          <p>Privacy policy</p>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <div>
+          <p> © {new Date().getFullYear()} AART, Inc. All Rights Reserved</p>
+        </div>
+        <div>
+          <a href="https://github.com/kaymen99">
+            <AiOutlineGithub size={22} color="white" className="footer-icon" />
+          </a>
+          <a href="https://github.com/kaymen99">
+            <AiOutlineTwitter size={22} color="white" className="footer-icon" />
+          </a>
+          <a href="https://github.com/kaymen99">
+            <RiDiscordFill size={22} color="white" className="footer-icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
