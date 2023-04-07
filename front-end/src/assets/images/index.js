d6d401f0d182e81373d0e8397fda0b002efed9d3
Aymen <AymenMir1001@gmail.com>
2023-04-07 23:06:10 +0100

    Update assets folder

---
+++ front-end/src/assets/images/index.js
@@ -0,0 +1,35 @@
+import logo from "./logo.png";
+import banner from "./banner-img.png";
+import user from "./profile.jpg";
+import service1 from "./service-1.png";
+import service2 from "./service-2.png";
+import service3 from "./service-3.png";
+import service4 from "./service-4.png";
+import nft1 from "./nft1.png";
+import nft2 from "./nft2.png";
+import nft3 from "./nft3.png";
+import nft4 from "./nft4.png";
+import dai from "./dai.png";
+import usdt from "./usdt.png";
+import usdc from "./usdc.png";
+import matic from "./matic.png";
+import profile_banner from "./profile_banner.png";
+
+export default {
+  logo,
+  banner,
+  user,
+  service1,
+  service2,
+  service3,
+  service4,
+  nft1,
+  nft2,
+  nft3,
+  nft4,
+  dai,
+  usdc,
+  usdt,
+  matic,
+  profile_banner,
+};
