9224d38b19479e7ac9abc14c3551d878667c63cb
Aymen <AymenMir1001@gmail.com>
2023-05-24 22:21:14 +0100

    Update SalePage.js

--- front-end/src/pages/SalePage.js
+++ front-end/src/pages/SalePage.js
@@ -1,9 +1,8 @@
-import "./../assets/styles/pages/salePage.css";
-import images from "../assets/images";
-
+import "../assets/styles/pages/salePage.css";
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { CircularProgress } from "@mui/material";
 import { AiOutlineClose } from "react-icons/ai";
 import { Modal } from "react-bootstrap";
 import { ethers } from "ethers";
@@ -12,34 +11,45 @@ import axios from "axios";
 import { IPFS_GATEWAY } from "../utils/ipfsStorage";
 import marketContract from "../artifacts/AARTMarket.sol/AARTMarket.json";
 import nftContract from "../artifacts/AARTCollection.sol/AARTCollection.json";
+import artistsContract from "../artifacts/AARTArtists.sol/AARTArtists.json";
 import {
   marketContractAddress,
+  artistsContractAddress,
   nftContractAddress,
   networkDeployedTo,
 } from "../utils/contracts-config";
 import networksMap from "../utils/networksMap.json";
 import { approveERC20 } from "../utils/exchange-utils";
-import { getTokenAddress, tokens } from "../utils/tokens-utils";
+import {
+  MATIC,
+  getTokenFromAddress,
+  formatTokenAmount,
+  tokens,
+  parseTokenAmount,
+} from "../utils/tokens-utils";
 
 const SalePage = () => {
+  let navigate = useNavigate();
   const { id } = useParams();
-  const wallet = useSelector((state) => state.blockchain.value);
+  const wallet = useSelector((state) => state.userData.value);
   const [loading, setLoading] = useState(false);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [paymentToken, setPaymentToken] = useState(0);
-
+  const [paymentToken, setPaymentToken] = useState(MATIC);
   const [saleInfo, setSaleInfo] = useState({
     tokenId: 0,
+    ownerInfo: "",
+    ownerAddress: "",
+    creatorInfo: "",
+    creatorAddress: "",
     name: "",
     description: "",
     imageUri: "",
-    creator: "",
     category: "",
     seller: "",
-    paymentToken: "",
+    paymentToken: MATIC,
     price: 0,
   });
 
@@ -63,22 +73,63 @@ const SalePage = () => {
       const sale = (await market_contract.getListings())[Number(id)];
       const tokenId = sale[0];
 
+      const tokenOwner = await nft_contract.ownerOf(tokenId);
       const tokenUri = await nft_contract.tokenURI(tokenId);
       const _metadata = await axios.get(
         tokenUri.replace("ipfs://", IPFS_GATEWAY)
       );
 
+      const _ownerInfo = await getUserProfile(tokenOwner);
+      const _creatorInfo = await getUserProfile(_metadata.data.creator);
+
+      const price = formatTokenAmount(wallet.network, sale[2], sale[3]);
+
       setSaleInfo({
         tokenId: Number(tokenId),
+        ownerInfo: _ownerInfo,
+        ownerAddress: tokenOwner,
+        creatorInfo: _creatorInfo,
+        creatorAddress: _metadata.data.creator,
         name: _metadata.data.name,
         description: _metadata.data.description,
         imageUri: _metadata.data.image.replace("ipfs://", IPFS_GATEWAY),
-        creator: _metadata.data.creator,
         category: _metadata.data.category,
         seller: sale[1],
         paymentToken: sale[2],
-        price: Number(sale[3]),
+        price: price,
       });
+    }
+  };
+
+  const getUserProfile = async (user) => {
+    if (wallet.network === networksMap[networkDeployedTo]) {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
+      const artists_contract = new ethers.Contract(
+        artistsContractAddress,
+        artistsContract.abi,
+        provider
+      );
+
+      const hasProfile = await artists_contract.hasProfile(user);
+      if (hasProfile) {
+        const userProfile = await artists_contract.getUserProfile(user);
+
+        const _metadata = await axios.get(
+          userProfile[1].replace("ipfs://", IPFS_GATEWAY)
+        );
+
+        return {
+          username: _metadata.data.username,
+          imageUri: _metadata.data.imageUri.replace("ipfs://", IPFS_GATEWAY),
+        };
+      }
+      return {
+        username: "username",
+        imageUri: "",
+      };
     }
   };
 
@@ -89,7 +140,6 @@ const SalePage = () => {
         "any"
       );
       const signer = provider.getSigner();
-
       const market_contract = new ethers.Contract(
         marketContractAddress,
         marketContract.abi,
@@ -98,22 +148,29 @@ const SalePage = () => {
 
       try {
         setLoading(true);
-        if (saleInfo.paymentToken !== 0) {
+        const _price = parseTokenAmount(
+          wallet.network,
+          saleInfo.paymentToken,
+          saleInfo.price
+        );
+        if (saleInfo.paymentToken !== ethers.constants.AddressZero) {
           await approveERC20(
             signer,
-            getTokenAddress(saleInfo.paymentToken),
+            saleInfo.paymentToken,
             marketContractAddress,
-            saleInfo.amount
+            _price
           );
           const buy_tx = await market_contract.buyItem(Number(id));
           await buy_tx.wait();
         } else {
           const buy_tx = await market_contract.buyItem(Number(id), {
-            value: saleInfo.amount,
+            value: _price,
           });
           await buy_tx.wait();
         }
         setLoading(false);
+
+        navigate(`/nft-page/${saleInfo.tokenId}`);
       } catch (err) {
         setLoading(false);
         handleClose();
@@ -129,15 +186,23 @@ const SalePage = () => {
         "any"
       );
       const signer = provider.getSigner();
-
       const market_contract = new ethers.Contract(
         marketContractAddress,
         marketContract.abi,
         signer
       );
 
-      const cancel_tx = await market_contract.cancelListing(Number(id));
-      await cancel_tx.wait();
+      try {
+        setLoading(true);
+        const cancel_tx = await market_contract.cancelListing(Number(id));
+        await cancel_tx.wait();
+        setLoading(false);
+
+        navigate(`/nft-page/${saleInfo.tokenId}`);
+      } catch (err) {
+        setLoading(false);
+        console.log(err);
+      }
     }
   };
 
@@ -160,28 +225,30 @@ const SalePage = () => {
             <div className="item-seller-creator-profile">
               <img
                 className="item-seller-creator-img"
-                src={images.user}
-                alt="profile image"
-              />
-              <div className="item-seller-creator-info">
-                <p>Creator</p>
-                <br />
-                <h4>Rian Leon </h4>
-              </div>
-            </div>
-            <div
-              className="item-seller-creator-profile"
-              style={{ paddingLeft: "25px" }}
-            >
-              <img
-                className="item-seller-creator-img"
-                src={images.user}
+                src={saleInfo.ownerInfo.imageUri}
                 alt="profile image"
               />
               <div className="item-seller-creator-info">
                 <p>Owner</p>
                 <br />
-                <h4>Rian Leon </h4>
+                <h4>{saleInfo.ownerInfo.username}</h4>
+              </div>
+            </div>
+            <div
+              className="item-seller-creator-profile"
+              style={{ paddingLeft: "20px" }}
+            >
+              <img
+                className="item-seller-creator-img"
+                src={saleInfo.creatorInfo.imageUri}
+                alt="profile image"
+              />
+              <div className="item-seller-creator-info">
+                <p>Creator</p>
+                <br />
+                <a href={`/creator-profile/${saleInfo.creatorAddress}`}>
+                  <h4>{saleInfo.creatorInfo.username}</h4>
+                </a>
               </div>
             </div>
           </div>
@@ -192,7 +259,12 @@ const SalePage = () => {
             <div className="sale_price">
               <small>Sale Price</small>
               <p>
-                {saleInfo.price} ETH <span>( ≈ $3,221.22)</span>
+                {saleInfo.price}{" "}
+                {
+                  getTokenFromAddress(wallet.network, saleInfo.paymentToken)
+                    .symbol
+                }{" "}
+                <span>( ≈ $3,221.22)</span>
               </p>
             </div>
           </div>
@@ -200,7 +272,11 @@ const SalePage = () => {
             {wallet.account !== saleInfo.seller ? (
               <>
                 <button className="primary-btn" onClick={buyItem}>
-                  Buy For {saleInfo.price} ETH
+                  {loading ? (
+                    <CircularProgress size={18} color="inherit" />
+                  ) : (
+                    "Buy"
+                  )}
                 </button>
                 <button
                   className="secondary-btn swapbox_select token_select"
@@ -208,19 +284,30 @@ const SalePage = () => {
                   onClick={() => {
                     handleShow();
                   }}
+                  disabled={true}
                 >
                   <img
                     className="token_img"
-                    src={tokens["Polygon Mainnet"][0].logo}
+                    src={
+                      getTokenFromAddress(wallet.network, saleInfo.paymentToken)
+                        .logo
+                    }
                   />
                   <span className="token_text">
-                    {tokens["Polygon Mainnet"][0].symbol}
+                    {
+                      getTokenFromAddress(wallet.network, saleInfo.paymentToken)
+                        .symbol
+                    }
                   </span>
                 </button>
               </>
             ) : (
               <button className="primary-btn" onClick={cancelSale}>
-                Cancel
+                {loading ? (
+                  <CircularProgress size={18} color="inherit" />
+                ) : (
+                  "Cancel"
+                )}
               </button>
             )}
           </div>
