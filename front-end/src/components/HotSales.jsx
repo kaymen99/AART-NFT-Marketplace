ded623b56bc3c56bd0e83ae2cca2ca80599552a9
Aymen <AymenMir1001@gmail.com>
2023-05-24 22:15:16 +0100

    Update HotSales.jsx

--- front-end/src/components/HotSales.jsx
+++ front-end/src/components/HotSales.jsx
@@ -6,7 +6,6 @@ import { useSelector } from "react-redux";
 import { ethers } from "ethers";
 import axios from "axios";
 import { IPFS_GATEWAY } from "../utils/ipfsStorage";
-import images from "../assets/images";
 import marketContract from "../artifacts/AARTMarket.sol/AARTMarket.json";
 import nftContract from "../artifacts/AARTCollection.sol/AARTCollection.json";
 import {
@@ -16,51 +15,8 @@ import {
 } from "../utils/contracts-config";
 import networksMap from "../utils/networksMap.json";
 
-const nft_items = [
-  {
-    name: "My Test NFT",
-    uri: images.nft1,
-    price: 90,
-  },
-  {
-    name: "My Test NFT 2",
-    uri: images.nft2,
-    price: 980,
-  },
-  {
-    name: "My Test NFT 3",
-    uri: images.nft3,
-    price: 70,
-  },
-  {
-    name: "My Test NFT 4",
-    uri: images.nft4,
-    price: 1000,
-  },
-  {
-    name: "My Test NFT",
-    uri: images.nft1,
-    price: 90,
-  },
-  {
-    name: "My Test NFT 2",
-    uri: images.nft2,
-    price: 980,
-  },
-  {
-    name: "My Test NFT 3",
-    uri: images.nft3,
-    price: 70,
-  },
-  {
-    name: "My Test NFT 4",
-    uri: images.nft4,
-    price: 1000,
-  },
-];
-
 const HotSales = () => {
-  const wallet = useSelector((state) => state.blockchain.value);
+  const wallet = useSelector((state) => state.userData.value);
   const [sales, setSales] = useState([]);
 
   const getHotSalesList = async () => {
@@ -74,7 +30,6 @@ const HotSales = () => {
         marketContract.abi,
         provider
       );
-
       const nft_contract = new ethers.Contract(
         nftContractAddress,
         nftContract.abi,
@@ -83,14 +38,12 @@ const HotSales = () => {
 
       const listings = await market_contract.getListings();
       const hottestBids = listings;
-
       const _sales = await Promise.all(
         hottestBids.map(async (item, index) => {
           const tokenUri = await nft_contract.tokenURI(item.tokenId);
           const metadata = await axios.get(
             tokenUri.replace("ipfs://", IPFS_GATEWAY)
           );
-
           return {
             id: index,
             tokenId: Number(item.tokenId),
@@ -118,9 +71,9 @@ const HotSales = () => {
         <div className="listing-title">
           <h1>Hot Bids</h1>
         </div>
-        {sales.length === 0 ? (
+        {sales.length !== 0 ? (
           <>
-            <Listing items={nft_items} />
+            <Listing items={sales} />
             <div className="load-more">
               <a href="/sales">
                 <button>See more</button>
