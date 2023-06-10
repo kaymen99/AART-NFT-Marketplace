7a8df31493297ca25a732d131f2111c8f973a263
Aymen <AymenMir1001@gmail.com>
2023-06-10 23:45:25 +0100

    Update helpers.js

--- front-end/src/utils/helpers.js
+++ front-end/src/utils/helpers.js
@@ -1,5 +1,8 @@
 import { ethers } from "ethers";
 
+export const defaultProfileImg =
+  "https://thumbs.dreamstime.com/b/profile-icon-black-background-graphic-web-design-modern-simple-vector-sign-internet-concept-trendy-symbol-profile-138113075.jpg";
+
 export function getAmountInWei(amount) {
   return ethers.utils.parseEther(amount.toString(), "ether");
 }
