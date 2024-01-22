cf74e5346a8af0944b1719971e74072f1ba41ee1
ROD ZISSO <muroko@users.noreply.github.com>
2024-01-22 10:54:30 -0500

    Frontend build issues fix
    Update contracts-config.js with missing "artistsContractAddress" and "mockDAIAddress" for the frontend to compile successfully.

--- front-end/src/utils/contracts-config.js
+++ front-end/src/utils/contracts-config.js
@@ -1,5 +1,7 @@
 
       export const marketContractAddress = "0x71Ac37334199bE2C10a6e74EF0636979692163d4"
+      export const artistsContractAddress = "0x473fB8B9ay6hB63784837b94f90e2B9a6b9p90b3"
       export const nftContractAddress = "0xa73fB8B9a88fB63784837b94f90e2B9a6b9734C1"
+      export const mockDAIAddress = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
       export const ownerAddress = "0xd4eFCA925505121D9039BE8DCFafaC862254A8be"
       export const networkDeployedTo = "1337"
